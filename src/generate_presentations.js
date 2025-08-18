5ca0aecbd1cf0939c3f89f958d4ca30c7c9c54e8
Dan Breczinski <pt2323@gmail.com>
2025-08-18 10:15:53 +0200

    fix build script

--- src/generate_presentations.js
+++ src/generate_presentations.js
@@ -1,10 +1,8 @@
-import { readFile, writeFile } from "fs/promises";
-import { Marp } from "@marp-team/marp-core";
 import { marpCli } from "@marp-team/marp-cli";
 import { readdir } from "fs/promises";
 
 const DATA_DIRECTORY = process.cwd() + "/presentations";
-const OUTPUT_DIRECTORY = process.cwd() + "/../presentations";
+const OUTPUT_DIRECTORY = process.cwd() + "\\..\\presentations";
 
 async function getFiles() {
   const fileMap = {};
@@ -19,27 +17,20 @@ async function getFiles() {
 (async () => {
   const files = await getFiles();
   for (const [name, filePath] of Object.entries(files)) {
-    await marpCli([filePath, "--pdf", "--allow-local-files"]);
-    await marpCli([filePath, "--html", "--allow-local-files"]);
-
-    // const content = await readFile(filePath, "utf-8");
-    // const marp = new Marp();
-    // const { html, css } = marp.render(content);
-    // const markup = `
-    // <!DOCTYPE html>
-    //     <html lang="en">
-    //     <head>
-    //     <meta charset="UTF-8">
-    //     <title>${name}</title>
-    //     <style>
-    //      ${css}
-    //     </style>
-    //     </head>
-    //     <body>
-    //         ${html}
-    //     </body>
-    //     </html>
-    // `;
-    // await writeFile(`${OUTPUT_DIRECTORY}/${name}.html`, markup);
+    console.log(name);
+    await marpCli([
+      filePath,
+      "--pdf",
+      "--allow-local-files",
+      "-o",
+      `${OUTPUT_DIRECTORY}/${name}.pdf`,
+    ]);
+    await marpCli([
+      filePath,
+      "--html",
+      "--allow-local-files",
+      "-o",
+      `${OUTPUT_DIRECTORY}/${name}.html`,
+    ]);
   }
 })();
